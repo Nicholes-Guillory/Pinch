3c9438092dd85813b564b4731dde329cfa889a1f
Nicholes Guillory <nicholes.guillory@gmail.com>
2022-12-13 13:39:29 -0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Nicholes Guillory on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
